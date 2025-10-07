e50f693eddd4577c0f7871d7d2938a4a5f9e798b
Javier <javierperezviloria07@gmail.com>
2025-10-07 11:10:57 -0500

    feat(models): add new model classes to `__init__` file
    - Include models in package exports:
        - AuditLog.
        - Category.
        - QuestionCategory.

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -1,4 +1,7 @@
+from .audit_log import AuditLog
+from .category import Category
 from .question import Question
+from .question_category import QuestionCategory
 from .response import Response
 from .role import Role
 from .survey import Survey
@@ -7,7 +10,10 @@ from .team import Team
 from .user import User
 
 __all__ = [
+    "AuditLog",
+    "Category",
     "Question",
+    "QuestionCategory",
     "Response",
     "Role",
     "Survey",
